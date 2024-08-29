f5a60bd668e670680a4da6aa59d7acf519c9d2f7
Sourav Saha <sahasourav123@gmail.com>
2024-08-29 20:11:39 +0530

    build(deps): add and upgrade dependencies

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -12,5 +12,5 @@ base_dir = os.path.dirname(os.getcwd())
 load_dotenv(f"{base_dir}/.env")
 load_dotenv(f"{base_dir}/.env.local", override=True)
 
-logging.basicConfig(level='INFO', format='%(message)s', datefmt="[%X]",  handlers=[RichHandler(keywords=['load-test', 'stress-test', 'baseline'])])
+logging.basicConfig(level='INFO', format='%(message)s', datefmt="[%X]",  handlers=[RichHandler()])
 logger = logging.getLogger()
