e1c468c880854b61ee94d868318cec1cee687257
Sourav Saha <sahasourav123@gmail.com>
2025-07-02 23:39:40 +0530

    fix: incorrect url displayed

--- frontend/src/constants.py
+++ frontend/src/constants.py
@@ -4,5 +4,6 @@ Created By: Sourav Saha
 """
 UPTIME_HISTORY_LIMIT = 60
 OUTCOME_HISTORY_LIMIT = 25
-PUSH_EVENT_ENDPOINT = "https://watchtower.finanssure.com/api/v1/public"
 SITE_URL = "https://watchtower.finanssure.com"
+PUSH_EVENT_ENDPOINT = "https://watchtower.finanssure.com/api/public/v1/push/event"
+CREATE_MONITOR_ENDPOINT = "https://watchtower.finanssure.com/api/external/v1/create/monitor"
